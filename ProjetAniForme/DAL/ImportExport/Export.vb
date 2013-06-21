2138c83a7ab6057a99974caf73f3fb64b23a5140
ShppeR <antho369@hotmail.fr>
2013-06-21 11:13:16 +0200

    import xml

--- ProjetAniForme/DAL/ImportExport/Export.vb
+++ ProjetAniForme/DAL/ImportExport/Export.vb
@@ -5,11 +5,13 @@ Imports System.Xml.Serialization
 Public Class Export
 
     Shared Sub exporterBaremes(ByVal list As List(Of Bareme), ByVal fichier As String)
+
         'Serialize object to a text file.
         Dim objStreamWriter As New StreamWriter(fichier)
         Dim x As New XmlSerializer(list.GetType)
         x.Serialize(objStreamWriter, list)
         objStreamWriter.Close()
+
     End Sub
 
 
--- ProjetAniForme/DAL/ImportExport/Import.vb
+++ ProjetAniForme/DAL/ImportExport/Import.vb
@@ -1,7 +1,33 @@
-﻿Public Class Import
+﻿Imports System.IO
+Imports BO
+Imports System.Xml.Serialization
+Imports System.Xml
 
-    Shared Function importerServices(ByVal fichier As String) As BO.Bareme()
-        Throw New NotImplementedException
+Public Class Import
+
+    Shared Function importerBaremes(ByVal fichier As String) As Bareme()
+        ' Declare an object variable of the type to be deserialized.
+        'Dim b As Bareme()
+        Dim x As New XmlSerializer(GetType(Bareme))
+
+        ' A FileStream is needed to read the XML document.
+        'Dim fs As New FileStream(fichier, FileMode.Open)
+        'Dim reader As New XmlTextReader(fs)
+
+        ' Use the Deserialize method to restore the object's state.
+        'b = CType(x.Deserialize(reader), Bareme())
+
+        'Return b
+
+
+        Dim objStreamReader As New StreamReader(fichier)
+        Dim p2 As Bareme()
+        p2 = CType(x.Deserialize(objStreamReader), Bareme())
+        objStreamReader.Close()
+
+        Return p2
+
     End Function
+
 
 End Class
